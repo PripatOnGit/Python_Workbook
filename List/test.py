2104a23bf2775631d50f2d15cdd6efd5f278181f
priyanka <patil.priyu26@gmail.com>
2026-08-07 12:43:19 +0530

    List problems

---
+++ List/test.py
@@ -0,0 +1,18 @@
+students = ['Dan','Alice','Bob','Carol','Dan', 'Eve','Frank','Dan']
+list_stud = [x for x in students if x!='Dan']
+
+print(list_stud)
+
+
+
+#students.append('Grace')
+#students.remove('Dan')
+#students.insert(2,'Zara')
+#print(students)
+
+#students.reverse()
+#print(students)
+
+#rev_students = students[::-1]
+#print(students)
+#print(rev_students)
